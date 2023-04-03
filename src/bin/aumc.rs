8c50e0468a94f646cb01e74cf4ce1a32cea5bc57
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-03 18:44:37 +0100

    Add initial code for library and the two binaries
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/bin/aumc.rs
+++ src/bin/aumc.rs
@@ -1,2 +1,21 @@
+//! This is the client to the `automon` daemon.
+#![deny(
+    warnings,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::pedantic,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
+
 fn main() {
+    unimplemented!()
 }
